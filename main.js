fca13008551675329b11b17fe69fcd85bd3cecdd
rafaxyzgamer <131929400+rafaxyzgamer@users.noreply.github.com>
2023-09-18 20:35:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,211 @@
+var nome = "Rafael Yuji " ;//12
+var sobrenome = "Ikawa Soares";//12
+
+var nomecompleto = nome.concat(sobrenome);
+console.log (nomecompleto);
+
+var comprimento = nomecompleto.length;
+console.log(comprimento);
+
+var mamae = nomecompleto.replace("Rafael Yuji", "Juliana Hatsumi");
+console.log(mamae);
+
+var mamae2 = mamae.replace("Soares","");
+console.log(mamae2);
+
+var lowerCase = mamae2.toLowerCase();
+console.log(lowerCase);
+
+var upperCase = nomecompleto.toUpperCase();
+console.log(upperCase);
+
+//Hora da sofrencia
+
+var a = 25;
+var b = 16;
+
+var raiz = Math.sqrt(b);
+console.log(raiz);
+
+var max = Math.max(a,b);
+console.log(max);
+
+var min = Math.min(a,b);
+console.log(min);
+var exp = Math.pow(a,b);
+console.log(exp);
+
+var c = 3.9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999;
+
+var doisDecimais = c.toFixed(2);
+console.log(doisDecimais);
+
+//fazer a soma  de a e b multiplicação multiplicação multiplicação ração e d i v i s ã o
+
+var add = a + b ;
+console.log(add);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
